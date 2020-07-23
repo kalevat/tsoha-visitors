c5cb5894473dad6e990c0f6a8265c47b98bda658
Antti Laaksonen <ahslaaks@cs.helsinki.fi>
2020-07-23 16:40:04 +0300

    Fix indentation

--- routes.py
+++ routes.py
@@ -4,46 +4,46 @@ import messages, users
 
 @app.route("/")
 def index():
-	list = messages.get_list()
-	return render_template("index.html", count=len(list), messages=list)
+    list = messages.get_list()
+    return render_template("index.html", count=len(list), messages=list)
 
 @app.route("/new")
 def new():
-	return render_template("new.html")
+    return render_template("new.html")
 
 @app.route("/send", methods=["post"])
 def send():
-	content = request.form["content"]
-	if messages.send(content):
-		return redirect("/")
-	else:
-		return render_template("error.html",message="Viestin lähetys ei onnistunut")
+    content = request.form["content"]
+    if messages.send(content):
+        return redirect("/")
+    else:
+        return render_template("error.html",message="Viestin lähetys ei onnistunut")
 
 @app.route("/login", methods=["get","post"])
 def login():
-	if request.method == "GET":
-		return render_template("login.html")
-	if request.method == "POST":
-		username = request.form["username"]
-		password = request.form["password"]
-		if users.login(username,password):
-			return redirect("/")
-		else:
-			return render_template("error.html",message="Väärä tunnus tai salasana")
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username,password):
+            return redirect("/")
+        else:
+            return render_template("error.html",message="Väärä tunnus tai salasana")
 
 @app.route("/logout")
 def logout():
-	users.logout()
-	return redirect("/")
+    users.logout()
+    return redirect("/")
 
 @app.route("/register", methods=["get","post"])
 def register():
-	if request.method == "GET":
-		return render_template("register.html")
-	if request.method == "POST":
-		username = request.form["username"]
-		password = request.form["password"]
-		if users.register(username,password):
-			return redirect("/")
-		else:
-			return render_template("error.html",message="Rekisteröinti ei onnistunut")
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.register(username,password):
+            return redirect("/")
+        else:
+            return render_template("error.html",message="Rekisteröinti ei onnistunut")
